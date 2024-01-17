683da501cbc16439faf10cd6b19e085cebb7af6c
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-17 19:44:51 +0100

    Rename database

--- bus_station/settings.py
+++ bus_station/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "bus_station.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db_bus_station.sqlite3",
     }
 }
 

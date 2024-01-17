70a85a0f9c5ae06ebd1adcc1cbe1d2994bccdde7
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-17 17:53:06 +0100

    Add 'POST' method to 'bus_list' view

--- station/views.py
+++ station/views.py
@@ -6,9 +6,18 @@ from station.models import Bus
 from station.serializers import BusSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def bus_list(request):
     if request.method == "GET":
         buses = Bus.objects.all()
         serializer = BusSerializer(buses, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = BusSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

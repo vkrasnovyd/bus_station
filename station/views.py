bbb6ff15314762b12678e3e225763982a1184cee
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-18 13:03:45 +0100

    Add 'bus_detail' api_view

--- station/views.py
+++ station/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -21,3 +22,24 @@ def bus_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def bus_detail(request, pk):
+    bus = get_object_or_404(Bus, id=pk)
+
+    if request.method == "GET":
+        serializer = BusSerializer(bus)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "PUT":
+        serializer = BusSerializer(bus, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == "DELETE":
+        bus.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+

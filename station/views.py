70508154224ef666eddbebb4204e2628181e8662
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-18 13:06:24 +0100

    Change serializer validation in 'bus_list' view

--- station/views.py
+++ station/views.py
@@ -17,11 +17,9 @@ def bus_list(request):
     if request.method == "POST":
         serializer = BusSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])

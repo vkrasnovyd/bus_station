6735b9ad54655232d661115c14b54769d4e6b18f
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-19 18:10:29 +0100

    Add TicketInline to Order admin view

--- station/admin.py
+++ station/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from station.models import Bus, Trip, Order, Ticket, Facility
 
 
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline, )
+
+
 admin.site.register(Bus)
 admin.site.register(Trip)
-admin.site.register(Order)
 admin.site.register(Ticket)
 admin.site.register(Facility)

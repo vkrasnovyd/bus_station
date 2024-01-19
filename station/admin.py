cfcb36cc477eac10ded457da0d15620092c376ae
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-19 18:00:13 +0100

    Register new models to admin view

--- station/admin.py
+++ station/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from station.models import Bus
+from station.models import Bus, Trip, Order, Ticket, Facility
+
 
 admin.site.register(Bus)
+admin.site.register(Trip)
+admin.site.register(Order)
+admin.site.register(Ticket)
+admin.site.register(Facility)

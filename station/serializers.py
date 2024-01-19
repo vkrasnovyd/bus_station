cb508e931554361fb00d8317194f7ed9ef0d1755
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-19 17:15:13 +0100

    Rewrite BusSerializer as ModelSerializer

--- station/serializers.py
+++ station/serializers.py
@@ -3,16 +3,9 @@ from rest_framework import serializers
 from station.models import Bus
 
 
-class BusSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    info = serializers.CharField(required=False)
-    num_seats = serializers.IntegerField()
+class BusSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        return Bus.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.info = validated_data.get("info", instance.info)
-        instance.num_seats = validated_data.get("num_seats", instance.num_seats)
-        instance.save()
-        return instance
+    class Meta:
+        model = Bus
+        fields = "__all__"
+        read_only_fields = ("id",)

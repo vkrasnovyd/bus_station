c73927ffbbc67d602f9f05d0116c2386e7063606
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-22 19:18:34 +0100

    Add view with User personal information

--- user/serializers.py
+++ user/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = get_user_model()
         fields = ("id", "username", "email", "password", "is_staff")
@@ -13,3 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update user with correctly encrypted password"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView, CreateTokenView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("login/", CreateTokenView.as_view(), name="token"),
     path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
